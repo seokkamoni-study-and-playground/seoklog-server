e035a1e37b48454787cf3906e27209d303a370bf
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-11 22:43:15 +0900

    SET :: 기본 세팅

---
+++ src/main/java/com/example/seoklog/SeoklogApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seoklog/SeoklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seoklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
